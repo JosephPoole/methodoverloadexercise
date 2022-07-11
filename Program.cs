431c0aedbea569ed04e10dff2898dd7c2b5f8416
Joseph Poole <Joseph.poole90@gmail.com>
2022-07-11 12:56:22 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Methodoverloadexercise;
+
+var a = 29;
+var b = 55;
+
+var answer = Method.Add(a, b);
+
+var c = 32.0m;
+var d = 45.0m;
+
+var decimalAnswer = Method.Add(c, d);
+
+var thirdAnswer = Method.Add(1, 0, true);
+
+Console.WriteLine($"int method add: {answer} decimal method add: {decimalAnswer}");
+Console.WriteLine(thirdAnswer);
+		
